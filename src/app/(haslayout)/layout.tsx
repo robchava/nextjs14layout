56800dcabc2b98bcf8ac45c7760cfe1e41dc5a90
Roberto Chavarria <robchava@gmail.com>
2023-11-09 10:54:28 +0100

    Update text

--- src/app/(haslayout)/layout.tsx
+++ src/app/(haslayout)/layout.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
-
 const Page = ({ children }: any) => {
   return (
     <div className="border p-6 m-6 bg-red-100">
       <h2>
-        I am in the /src/app/<b>(haslayout)</b>/layout.tsx, and I am a ROOT layout (I have my own html & body tags)!
+        I am in the /src/app/<b>(haslayout)</b>/layout.tsx
       </h2>
       <br />
       <div className="border p-6 m-6">{children}</div>
